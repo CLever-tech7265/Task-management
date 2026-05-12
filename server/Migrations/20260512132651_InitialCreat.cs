a217523eed76e776872cf5ecbde032bd2fb7e8ca
malki <tichnut91@8547900.org.il>
2026-05-12 16:27:53 +0300

    yom shlishi

---
+++ server/Migrations/20260512132651_InitialCreat.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeLevel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeLevel = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeLevel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shifts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartHour = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FinishHour = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shifts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specialization",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Spec = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialization", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PeopleId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_EmployeeLevel_Level",
+                        column: x => x.Level,
+                        principalTable: "EmployeeLevel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShiftSpecialization",
+                columns: table => new
+                {
+                    SpecsId = table.Column<int>(type: "int", nullable: false),
+                    shiftId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShiftSpecialization", x => new { x.SpecsId, x.shiftId });
+                    table.ForeignKey(
+                        name: "FK_ShiftSpecialization_Shifts_shiftId",
+                        column: x => x.shiftId,
+                        principalTable: "Shifts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShiftSpecialization_Specialization_SpecsId",
+                        column: x => x.SpecsId,
+                        principalTable: "Specialization",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeSpecialization",
+                columns: table => new
+                {
+                    SpecId = table.Column<int>(type: "int", nullable: false),
+                    employeesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeSpecialization", x => new { x.SpecId, x.employeesId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeSpecialization_Employees_employeesId",
+                        column: x => x.employeesId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeSpecialization_Specialization_SpecId",
+                        column: x => x.SpecId,
+                        principalTable: "Specialization",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Level",
+                table: "Employees",
+                column: "Level");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeSpecialization_employeesId",
+                table: "EmployeeSpecialization",
+                column: "employeesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShiftSpecialization_shiftId",
+                table: "ShiftSpecialization",
+                column: "shiftId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeSpecialization");
+
+            migrationBuilder.DropTable(
+                name: "ShiftSpecialization");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Shifts");
+
+            migrationBuilder.DropTable(
+                name: "Specialization");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeLevel");
+        }
+    }
+}
