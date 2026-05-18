50d9aa2b3fc6bbd3f0715c117fbdf73d4c010dab
malki <tichnut91@8547900.org.il>
2026-05-18 14:16:28 +0300

    to miri

--- server/Controllers/ShiftController.cs
+++ server/Controllers/ShiftController.cs
@@ -14,7 +14,7 @@ namespace TaskManagement.Controllers{
             _context=context;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Shift>>> GetShifts(int skip=0,int take=2)
+        public async Task<ActionResult<IEnumerable<Shift>>> GetShifts(int skip=1,int take=7)
         {
             return await _context.Shifts.Skip(skip).Take(take).ToListAsync();
         }
@@ -36,6 +36,47 @@ namespace TaskManagement.Controllers{
             _context.Shifts.Add(shift);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetShifts),shift) ;
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Shift>> UpdateShift(Guid id,string StartHour,string FinishHour)
+        {
+            // if(id!= shiftdto.Id)
+            // {
+            //     return BadRequest();
+            // }
+            if (!ShiftExists(id))
+            {
+                return NotFound();
+            }
+            var shiftEntity=await _context.Shifts.FindAsync(id) ;
+            shiftEntity.StartHour=StartHour.Trim() ?? shiftEntity.StartHour;
+            shiftEntity.FinishHour=FinishHour.Trim() ?? shiftEntity.FinishHour;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShift(Guid id)
+        {
+            var shift=await _context.Shifts.FindAsync(id);
+            if(shift==null)
+            {
+                return NotFound();
+            }
+            _context.Shifts.Remove(shift);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+        private bool ShiftExists(Guid id)
+        {
+            return _context.Shifts.Any(e=>e.Id==id);
         }
     }
 }
