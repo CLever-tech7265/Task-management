461fdd2b2f84a37a67a79156aa1d18e3b51e0ec9
Miri_nagel <tichnut85@.org.il>
2026-05-31 15:49:17 +0300

    baruch h

--- server/Model/Employee.cs
+++ server/Model/Employee.cs
@@ -8,7 +8,7 @@ namespace TaskManagement.modules{
         
         public Guid  Id { get; set; }
         public string EmployeeLevel { get; set; }="";
-       // public List<Employee> Employees { get; set; }
+       public List<Employee> Employees { get; set; } = new();
     }
     public class Employee{
        
@@ -18,9 +18,18 @@ namespace TaskManagement.modules{
       public string PeopleId { get; set; }="";
       public DateTime BirthDate { get; set; }
       public string Email { get; set; }="";
-    public List<Specialization> Spec { get; set; }=[];
-    [ForeignKey("Level")]
-    public Level IdOfLevel { get; set; }
+    public List<Specialization>? Specs { get; set; }=new();
+    
+    public Guid? LevelId { get; set; }
+    public Level? Level { get; set; }
+    }
+
+    public class EmployeeDto{
+      public string FirstName { get; set; }="";
+      public string LastName { get; set; }="";
+      public string PeopleId { get; set; }="";
+      public DateTime BirthDate { get; set; }
+      public string Email { get; set; }="";
     }
  
 }
